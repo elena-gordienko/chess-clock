207b564e537d7afb987db352d1e527c9eba8c32c
Elena Gordienko <gloomylego@gmail.com>
2022-03-29 22:52:35 +0400

    add SwiftUI project

---
+++ ChessClock/ChessClock/ChessClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessClockApp.swift
+//  ChessClock
+//
+//  Created by Elena Gordienko on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChessClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessClock/ChessClock/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChessClock
+//
+//  Created by Elena Gordienko on 29.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
