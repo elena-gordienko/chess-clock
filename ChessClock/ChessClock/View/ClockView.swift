7d995ede40285b73f17f96e1f8e56f13a6c0a9e2
Elena Gordienko <gloomylego@gmail.com>
2022-04-03 21:50:53 +0400

    add watchfaces to clock view

--- ChessClock/ChessClock/View/ClockView.swift
+++ ChessClock/ChessClock/View/ClockView.swift
@@ -13,15 +13,66 @@ struct ClockView: View {
     
     @State var totalTime: Seconds
     
+    @State var remainingTimeWhite: Seconds
+    @State var remainingTimeBlack: Seconds
+    
+    let timer = Timer
+        .publish(every: 1, on: .main, in: .common)
+        .autoconnect()
+    
     init(gameState: GameState, totalTime: Seconds) {
         _gameState = StateObject(wrappedValue: gameState)
         self.totalTime = totalTime
+        remainingTimeWhite = totalTime
+        remainingTimeBlack = totalTime
     }
     
     var body: some View {
-        Text("Hello")
+        ZStack {
+            Configuration.backgroundColor.ignoresSafeArea()
+            HStack(alignment: .center) {
+                whiteWatch
+                Spacer(minLength: 20)
+                blackWatch
+            }
+            .padding()
+        }
+        .onReceive(timer) { _ in
+            handleTimerEvent()
+        }
     }
     
+    
+    var whiteWatch: some View {
+        WatchfaceView(
+            remainingTime: $remainingTimeWhite,
+            isActive: $gameState.isWhiteTurn
+        )
+    }
+
+    var blackWatch: some View {
+        WatchfaceView(
+            remainingTime: $remainingTimeBlack,
+            isActive: $gameState.isBlackTurn
+        )
+    }
+
+    private func handleTimerEvent() {
+        guard case let .inProgress(isWhite) = gameState.state else { return }
+        let remainingTime = isWhite ? remainingTimeWhite : remainingTimeBlack
+        guard remainingTime > 0 else {
+            gameState.state = .timeRanOut
+            return
+        }
+        if isWhite {
+            remainingTimeWhite -= 1
+        } else {
+            remainingTimeBlack -= 1
+        }
+    }
+    enum Configuration {
+        static let backgroundColor = Color.Palette.background
+    }
 }
 
 struct ClockView_Previews: PreviewProvider {
